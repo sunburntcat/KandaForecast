7e1e244267a906e9c6008aae1bb166144b286085
sunburntcat <nick_lopez_777@hotmail.com>
2021-11-11 10:13:12 -0600

    Remove trailing comma in CSV file

--- listen_for_launch.js
+++ listen_for_launch.js
@@ -8,8 +8,12 @@ const csv = require('./lib/radiosonde_csv_writer.js');
     // Define function of what to do when new data is received.
     const func = async (data, ack) => {
 
+        //let contract = data.content.data.contract; Ideally we want to know which contract it came from
         let launch_id = data.content.data.launch_id;
         let level = data.content.data.pressure_hpa;
+
+        // Helpful to watch progress of balloon
+        console.log(level);
 
         const target = 500;
 
@@ -25,12 +29,14 @@ const csv = require('./lib/radiosonde_csv_writer.js');
             // Run python ML model using the CSV file
             console.log("Initiating python script, but not waiting.");
 
-
+            // Request the connection be closed
             return true;
 
+        } else {
+            // Target level not yet reached so keep listening
+            return false;
         }
 
-        return false;
     };
 
     await radiosondes.listen_ascensionwx_once( func, contract );
