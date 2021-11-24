dbdec466d5a4f938584c14180f44610bea34fdf2
sunburntcat <nick_lopez_777@hotmail.com>
2021-11-24 10:52:45 -0600

    Added comments and hints

--- examples/dclimate/listener_dclimateiot2.js
+++ examples/dclimate/listener_dclimateiot2.js
@@ -9,24 +9,22 @@ const listener = require(root_dir + 'lib/eosio_listeners');
     // Define function of what to do when new data is received.
     const func = async (data, ack) => {
         /*
+            This is a callback function that instructs nodejs what to do when data
+            is added to an eosio table.
         NOTE:
-            When used with listen_eosio_table, this function should return false, if
-                the user would like to continue listening to the table
-            If the incoming data meets your criteria to stop listening, then you
-                should return true and the listner object will close the websocket.
+            Note if you would like to stop listening to the blockchain when your data
+            meets a certain criteria, return true on this function and the connection will be closed.
         */
 
-        //let launch_id = data.content.data.launch_id;
-
-        // Prints out the blockchain state change
+        // Prints out the data submitted to the chain from the iot device.
         console.log(data.content.data);
 
         // Here, dclimate can call a nodejs package that submits the data to IPFS
         //  e.g. https://js.ipfs.io/
 
-        return false;
     };
 
+    // Begin actually watching the eosio blockchain table called "observations"
     await listener.watch_table( func, contract, "observations" );
 
 })();
